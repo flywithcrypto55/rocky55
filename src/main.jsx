e2b5b510f5346f667f0c842445115dd642ef9a67
5rahulmangla <5rahulmangla@gmail.com>
2025-05-16 12:00:54 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// insertCoin({
-//   skipLobby:true,
-// }).then(()=>
+insertCoin({
+  skipLobby:true,
+}).then(()=>
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-// );
+);
