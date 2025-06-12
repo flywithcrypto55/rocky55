5015c0dd9c1fdb236c0cbc3b347c2c92f1282a03
5rahulmangla <5rahulmangla@gmail.com>
2025-06-12 13:16:44 +0530

    testnenew var23

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,15 @@ import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 function App() {
   return (
     <>
+
+    <h1>env value is : {process.env.REACT_APP_NEW_VAR}</h1>
       <Router>
     <Routes>
       <Route path="/" element={<Page/>} />
        <Route path="/game" element={<GameApp/>} /> 
-
       {/* <Route path="/home" element={<AppHome/>} /> */}
-    </Routes>
-</Router>
+      </Routes>
+    </Router>
     </>
   )
 }
