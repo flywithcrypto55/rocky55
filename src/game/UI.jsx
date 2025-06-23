50d7200b23503bb1d5ebc9b2d3b3d0b2ea175dd1
flywithcrypto55 <flywithcrypto55@gmail.com>
2025-06-23 23:57:43 +0530

    Update UI.jsx

--- src/game/UI.jsx
+++ src/game/UI.jsx
@@ -20,7 +20,7 @@ export const UI = () => {
       </h2>
       )}
 
-        <a href="https://dexscreener.com/solana/48jg8os6fhddahz7j2ajru2tjrvdjn3mwuzjraskfcuy">
+        <a href="https://dexscreener.com/solana/FUsgsF7xrARatNrovUPzZAgaVLqrFxLykRb3ePRm9hsD">
         <img src="images/DexLogo.png" className="pointer-events-auto absolute top-8 right-20 w-4 h-4 transition duration-300 hover:scale-110 hover:rotate-5 hover:opacity-80" />
         </a>
 
